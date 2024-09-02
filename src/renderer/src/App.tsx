9f634204a4161209263ea4a89fcf8589e14bef92
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-09-02 10:59:01 -0300

    esqueleto da aplicação

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,16 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
 export function App() {
   return (
-    <div className="h-screen w-screen bg-rotion-900 text-rotion-100"></div>
+    <div className="h-screen w-screen bg-rotion-900 text-rotion-100 flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header />
+        <main className="flex-1 flex items-center justify-center text-rotion-400">
+          Selecione ou um crie um documento
+        </main>
+      </div>
+    </div>
   );
 }
