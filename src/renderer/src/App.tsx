df94dd297eb6b65eeb8c9779098eabec66e6b364
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-09-02 11:26:36 -0300

    configurando layout

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,15 +1,7 @@
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
-import { Routes } from "./routes";
+import { Routes } from "./Routes";
 
 export function App() {
-  return (
-    <div className="h-screen w-screen bg-rotion-900 text-rotion-100 flex">
-      <Sidebar />
-      <div className="flex-1 flex flex-col max-h-screen">
-        <Header />
-        <Routes />
-      </div>
-    </div>
-  );
+  return <Routes />;
 }
