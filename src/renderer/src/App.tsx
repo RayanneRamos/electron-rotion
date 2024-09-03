89ce82ba9fc42eff4fe8c0d83d213ae57f7da443
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-09-03 16:13:41 -0300

    configurando o react query

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,11 @@
-import { Header } from "./components/Header";
-import { Sidebar } from "./components/Sidebar";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Routes } from "./Routes";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
-  return <Routes />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Routes />
+    </QueryClientProvider>
+  );
 }
