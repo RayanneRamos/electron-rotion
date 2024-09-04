ed8b03967b48c0737eaeacf7524a63db69f70130
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-09-04 15:37:19 -0300

    estruturando IPC handlers

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,14 +2,19 @@ import path from "node:path";
 import { defineConfig, externalizeDepsPlugin } from "electron-vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
+import tsconfigPathsPlugin from "vite-tsconfig-paths";
+
+const tsconfigPaths = tsconfigPathsPlugin({
+  projects: [path.resolve("tsconfig.json")],
+});
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [tsconfigPaths, externalizeDepsPlugin()],
     publicDir: path.resolve("resources"),
   },
   preload: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [tsconfigPaths, externalizeDepsPlugin()],
   },
   renderer: {
     define: {
@@ -29,6 +34,6 @@ export default defineConfig({
         "@renderer": path.resolve("src/renderer/src"),
       },
     },
-    plugins: [react()],
+    plugins: [tsconfigPaths, react()],
   },
 });
