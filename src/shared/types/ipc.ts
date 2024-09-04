12e29626ae366dfa48ee376872bb9ff3e27128ad
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-09-04 15:48:07 -0300

    estrutura do CRUD via IPC

--- src/shared/types/ipc.ts
+++ src/shared/types/ipc.ts
@@ -1,9 +1,27 @@
 export interface Document {
   id: string;
   title: string;
-  content: string;
+  content?: string;
 }
+
+export interface FetchDocumentRequest {
+  id: string;
+}
+
+export interface DeleteDocumentRequest {
+  id: string;
+}
+
+export interface SaveDocumentRequest extends Document {}
 
 export interface FetchAllDocumentsResponse {
   data: Document[];
+}
+
+export interface FetchDocumentsResponse {
+  data: Document;
+}
+
+export interface CreateDocumentResponse {
+  data: Document;
 }
