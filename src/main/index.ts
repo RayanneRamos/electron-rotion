303e4d90f87345f9fb68dec0f2b6f9d30f1d8aed
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-09-06 15:20:17 -0300

    configurando hotkeys

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,6 +7,7 @@ import "./ipc";
 import "./store";
 import "./tray.ts";
 import { createTray } from "./tray";
+import { createShortcuts } from "./shortcuts";
 
 function createWindow(): void {
   // Create the browser window.
@@ -29,6 +30,7 @@ function createWindow(): void {
   });
 
   createTray(mainWindow);
+  createShortcuts(mainWindow);
 
   mainWindow.on("ready-to-show", () => {
     mainWindow.show();
